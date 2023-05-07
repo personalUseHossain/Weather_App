3eb2afae61622f41535eeb42ecb6b87f988e375c
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-05-07 17:48:31 +0600

    loading function added with js

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 const getWeatherBtn = document.getElementById("btn");
 const Input = document.getElementById("input");
 const weatherShow = document.querySelector(".weather-show");
+const loadingAnimation = document.querySelector(".loading-animation");
 
 //EventListenr for the Get weather BTN
 getWeatherBtn.addEventListener("click", () => {
+  //loading animation start when reading data from api
+  loadingAnimation.classList.remove("none");
+
   //Getting city name
   const cityName = Input.value;
-  weatherShow.style.display = "block";
+  weatherShow.style.display = "grid";
 
   //Fetching data from API
   async function fetchData() {
@@ -26,9 +30,23 @@ getWeatherBtn.addEventListener("click", () => {
     const temp = document.getElementById("tem");
     const humid = document.getElementById("humid");
     const wind = document.getElementById("wind-speed");
+    const feelTemp = document.getElementById("feel");
     temp.innerHTML = data.temp + "C";
     humid.innerHTML = data.humidity + "%";
     wind.innerHTML = data.wind_speed + "KM";
+    feelTemp.innerHTML = data.feels_like + "C";
+
+    //making appear the weather-show section when request for weather other wise this section won't be showed to users
+    weatherShow.style.display = "grid";
+
+    //Loading animation end when reading data from api is done
+    loadingAnimation.classList.add("none");
   }
+  //caling the function for getting weather and setting it up on dom
   fetchData();
+});
+
+//event listener on window object for loading animation to start
+window.addEventListener("load", () => {
+  loadingAnimation.classList.add("none");
 });
